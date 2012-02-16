fd83a3342ee639e051194ddb20e1ee9281c81b23
Biju Chacko <biju.ch@directi.com>
2012-02-16 15:19:13 +0530

    Basic config file parsing in place.

---
+++ lib/varanus/config.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+module Varanus
+  class Config
+
+    attr_accessor :directory
+    
+    def initialize(args=nil)
+      raise Error if not args.nil? and args.class != Array
+      set_defaults
+      parse_options args unless args.nil?
+      parse_config
+    end
+
+    def set_defaults
+      @data = Hash.new
+      self.directory = "/etc/varanus"
+    end
+    
+    def parse_options(args)
+      opts = OptionParser.new do |opts|
+        opts.on('-d', '--config-dir DIRECTORY',
+                'Look in DIRECTORY for configuration') do |dir|
+          self.directory = dir
+        end
+
+        opts.on_tail('-h', '--help',
+                'Print this message') do |dir|
+          puts opts
+          exit
+        end
+      end
+      opts.parse!(args) 
+    end
+
+    def parse_config
+      pattern = File.join(self.directory, "*.json")
+      Dir.glob(pattern) do |filename|
+        content = File.open(filename, "r") { |f| f.read }
+        config = JSON.parse content
+        config.each_pair do |key,value|
+          if self.has_key? key
+            if self[key].class == Array
+              if value.class == Array
+                self[key] += value
+              else
+                self[key] << value
+              end
+            else
+              self[key] = [self[key], value]
+            end
+          else
+            self[key] = value
+          end
+        end
+      end
+    end
+
+    def [](key)
+      @data[key]
+    end
+
+    def []=(key, value)
+      @data[key] = value
+    end
+
+    def has_key?(key)
+      @data.has_key?(key)
+    end
+  end
+end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -3,49 +3,112 @@
 libdir = File.expand_path("../../lib", __FILE__)
 $: << libdir unless $:.include? libdir
 
-require 'varanus/config_manager'
+require 'varanus/config'
+require 'json'
+require 'fileutils'
+
 require "spec_helper.rb"
 
-describe Varanus::ConfigManager do
+
+describe Varanus::Config do
   it "can be instantiated" do
-    expect { c = Varanus::ConfigManager.new() }.to_not raise_error
+    expect { c = Varanus::Config.new() }.to_not raise_error
   end
 
   it "can be instantiated with an argument" do
     args = Array.new
-    expect { c = Varanus::ConfigManager.new(args) }.to_not raise_error    
+    expect { c = Varanus::Config.new(args) }.to_not raise_error    
   end
 
   it "should only accept an array as an argument" do
     args = ""
-    expect { c = Varanus::ConfigManager.new(args) }.to raise_error    
+    expect { c = Varanus::Config.new(args) }.to raise_error    
   end
 
-  it "should accept --config-dir argument" do
-    args = ["--config-dir"]
-    expect { c = Varanus::ConfigManager.new(args) }.to_not raise_error(OptionParser::InvalidOption) 
+  describe "Option Parsing" do
+    it "should accept --config-dir argument" do
+      args = ["--config-dir"]
+      expect { c = Varanus::Config.new(args) }.to_not raise_error(OptionParser::InvalidOption) 
+    end
+
+    # suppress noise during test
+    xit "should accept --help argument" do
+      args = ["--help"]
+      expect { c = Varanus::Config.new(args) }.to_not raise_error(OptionParser::InvalidOption)    
+    end
+
+    it "should not accept any other argument" do
+      args = ["--foo"]
+      expect { c = Varanus::Config.new(args) }.to raise_error(OptionParser::InvalidOption) 
+    end
+
+    it "should set the directory attribute to '/etc/varanus' if it's not passed as an option" do
+      c = Varanus::Config.new
+      c.directory.should == '/etc/varanus'
+    end
+
+    it "should set the directory attribute to whaetever is passed as an option" do
+      args = ["--config-dir", "boo"]
+      c = Varanus::Config.new args
+      c.directory.should == 'boo'
+    end
   end
 
-  it "should accept --help argument" do
-    args = ["--help"]
-    expect { c = Varanus::ConfigManager.new(args) }.to_not raise_error(OptionParser::InvalidOption)    
-  end
+  describe "Config File Parsing" do
+    let(:test_data_dir) { File.expand_path("../test_data", __FILE__) }
 
-  it "should not accept any other argument" do
-    args = ["--foo"]
-    expect { c = Varanus::ConfigManager.new(args) }.to raise_error(OptionParser::InvalidOption) 
-  end
+    before(:each) do
+      FileUtils.mkdir_p test_data_dir
+    end
+    
+    after(:each) do
+      FileUtils.rm_rf test_data_dir
+    end
+    
+    it "should add a file in the the config dir to the config" do
+      content = { 'foo' => 'bar' }
+      create_file("foo.json", content.to_json)
+      c = Varanus::Config.new ["-d", test_data_dir]
+      c['foo'].should == 'bar'
+    end
 
-  it "should set the directory attribute to '/etc/varanus' if it's not passed as an option" do
-    c = Varanus::ConfigManager.new
-    c.directory.should == '/etc/varanus'
-  end
+    it "should merge two values into an array" do
+      content = { 'foo' => 'bar' }
+      create_file("foo.json", content.to_json)
+      content2 = { 'foo' => 'bar2' }
+      create_file("foo2.json", content2.to_json)
+      
+      c = Varanus::Config.new ["-d", test_data_dir]
+      c['foo'].should == [ 'bar', 'bar2' ]
+    end
 
-  it "should set the directory attribute to whaetever is passed as an option" do
-    args = ["--config-dir", "boo"]
-    c = Varanus::ConfigManager.new args
-    c.directory.should == 'boo'
+    it "should merge multiple values into an array" do
+      content = { 'foo' => 'bar' }
+      create_file("foo.json", content.to_json)
+      content2 = { 'foo' => 'bar2' }
+      create_file("foo2.json", content2.to_json)
+      content3 = { 'foo' => 'bar3' }
+      create_file("foo3.json", content3.to_json)
+      
+      c = Varanus::Config.new ["-d", test_data_dir]
+      c['foo'].should == [ 'bar', 'bar2', 'bar3' ]
+    end
+    
+    it "should merge multiple arrays into an array" do
+      content = { 'foo' => [1, 2, 3] }
+      create_file("foo.json", content.to_json)
+      content2 = { 'foo' => [4, 5, 6] }
+      create_file("foo2.json", content2.to_json)
+      
+      
+      c = Varanus::Config.new ["-d", test_data_dir]
+      c['foo'].should == [1, 2, 3, 4, 5, 6]
+    end
+    
+    def create_file(name, content)
+      file_path = File.join(test_data_dir, name)
+      File.open(file_path, 'w') {|f| f.write(content) }
+    end
   end
 end
-
 
